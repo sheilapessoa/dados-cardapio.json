24161f297a0934d62de15d4f93d6a08486ca78a8
sheilapessoa <contatomenudigita@gmail.com>
2026-01-14 21:33:31 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,34 @@
-const CACHE_NAME = 'menu-v1';
-const STATIC_FILES = [
-'/',
-'/index.html',
-'/manifest.json'
-];
+const CACHE = 'menu-v1';
 
-
-// Instalação
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
-);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ])
+    )
+  );
+  self.skipWaiting();
 });
 
-
-// Ativação
-self.addEventListener('activate', event => {
-event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-)
-);
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
 });
 
-
-// Fetch – estratégia iFood (network first)
-self.addEventListener('fetch', event => {
-const url = event.request.url;
-
-
-// JSON do cardápio sempre tenta internet
-if (url.includes('cardapio.json')) {
-event.respondWith(
-fetch(event.request)
-.then(res => {
-const clone = res.clone();
-caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
-return res;
-})
-.catch(() => caches.match(event.request))
-);
-return;
-}
-
-
-// Demais arquivos
-event.respondWith(
-fetch(event.request).catch(() => caches.match(event.request))
-);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
